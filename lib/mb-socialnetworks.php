a0c074075f10e0ffab70a82e13d5de8873e6db91
mechabyte <matt@mechabyte.com>
2014-08-01 20:18:35 -0400

    Facebook Graph API

---
+++ lib/mb-socialnetworks.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: (MB) Social Networks
+Plugin URI: http://www.mechabyte.com
+Description: A widget that allows you to grab stats from popular social networks
+Version: 1.00
+Author: Mechabyte - Matthew Smith
+Author URI: http://www.mechabyte.com
+*/
+
+/**
+ * mechabyteYouTube Class
+ */
+
+class mechabyteSocialNetworks {
+	public function facebook( $pageId, $piece ) {
+		$cache = 60*60;
+		$transient_id = 'mechabyteSocialFacebook';
+		$cached_item = get_transient ( $transient_id );
+		if ( !$cached_item || ($cached_item->pageId != $pageId ) ) {
+
+			$api_url = "http://graph.facebook.com/%s";
+			$response = wp_remote_get( sprintf($api_url, $pageId) );
+
+			if ( is_wp_error( $response ) or ( wp_remote_retrieve_response_code( $response ) != 200 ) ) {
+				return false;
+			}
+
+			$item_data = json_decode( wp_remote_retrieve_body( $response ), true );
+			
+			// If our info isn't in an array then these next steps won't work-- return false
+			if ( !is_array( $item_data ) ) {
+				return false;
+			}
+
+			// Create an object with our data
+			$data_to_cache = new stdClass();
+			$data_to_cache->pageId = $pageId;
+			$data_to_cache->item_info = $item_data;
+			// Store our data object as an item in the 'mechabyte_YouTube' transient
+			set_transient( $transient_id, $data_to_cache, $cache );
+			
+			// Return the data we found
+			return $item_data[$piece];
+		}
+		return $cached_item->item_info[$piece];
+	}
+}
+
+global $mechabyteSocialNetworks;
+$mechabyteSocialNetworks = new mechabyteSocialNetworks();
+
+?>
--- template-home.php
+++ template-home.php
@@ -9,64 +9,101 @@ Template Name: Homepage Template
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<div class='col-md-offset-2 col-md-8'>
-      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
-        <!-- Bottom Carousel Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
-          <li data-target="#quote-carousel" data-slide-to="1"></li>
-          <li data-target="#quote-carousel" data-slide-to="2"></li>
-        </ol>
-        
-        <!-- Carousel Slides / Quotes -->
-        <div class="carousel-inner">
-        
-          <!-- Quote 1 -->
-          <div class="item active">
-            <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
-                  <small>Someone famous</small>
-                </div>
-              </div>
-            </blockquote>
-          </div>
-          <!-- Quote 2 -->
-          <div class="item">
-            <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/mijustin/128.jpg" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
-                  <small>Someone famous</small>
-                </div>
-              </div>
-            </blockquote>
-          </div>
-          <!-- Quote 3 -->
-          <div class="item">
-            <blockquote>
-              <div class="row">
-                <div class="col-sm-3 text-center">
-                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/keizgoesboom/128.jpg" style="width: 100px;height:100px;">
-                </div>
-                <div class="col-sm-9">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
-                  <small>Someone famous</small>
-                </div>
-              </div>
-            </blockquote>
-          </div>
-        </div>
-        
-        <!-- Carousel Buttons Next/Prev -->
-        <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
-        <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
-      </div>                          
+<?php 
+
+echo json_encode( mechabyteSocialNetworks::facebook(117978191570403,'likes') );
+
+?>
+
+
+<div class="col-md-4">
+    <div class="list-group social-box">
+        <a href="http://www.jquery2dotnet.com" class="list-group-item facebook-like">
+            <h3 class="pull-right">
+                <i class="fa fa-facebook-square"></i>
+            </h3>
+            <h4 class="list-group-item-heading count">
+                1,694</h4>
+            <p class="list-group-item-text">
+                Like Us on Facebook</p>
+        </a><a href="http://www.jquery2dotnet.com" class="list-group-item twitter">
+            <h3 class="pull-right">
+                <i class="fa fa-twitter-square"></i>
+            </h3>
+            <h4 class="list-group-item-heading count">
+                344</h4>
+            <p class="list-group-item-text">
+                Follow Us on Twitter</p>
+        </a>
+        <a href="http://www.jquery2dotnet.com" class="list-group-item youtube">
+                        <h3 class="pull-right">
+                            <i class="fa fa-youtube-play"></i>
+                        </h3>
+                        <h4 class="list-group-item-heading count">
+                            X,XXX</h4>
+                        <p class="list-group-item-text">
+                            Watch Our YouTube Videos</p>
+                    </a>
     </div>
+</div>
+<div class='col-md-8'>
+	      <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+	        <!-- Bottom Carousel Indicators -->
+	        <ol class="carousel-indicators">
+	          <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+	          <li data-target="#quote-carousel" data-slide-to="1"></li>
+	          <li data-target="#quote-carousel" data-slide-to="2"></li>
+	        </ol>
+	        
+	        <!-- Carousel Slides / Quotes -->
+	        <div class="carousel-inner">
+	        
+	          <!-- Quote 1 -->
+	          <div class="item active">
+	            <blockquote>
+	              <div class="row">
+	                <div class="col-sm-3 text-center">
+	                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" style="width: 100px;height:100px;">
+	                </div>
+	                <div class="col-sm-9">
+	                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
+	                  <small>Someone famous</small>
+	                </div>
+	              </div>
+	            </blockquote>
+	          </div>
+	          <!-- Quote 2 -->
+	          <div class="item">
+	            <blockquote>
+	              <div class="row">
+	                <div class="col-sm-3 text-center">
+	                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/mijustin/128.jpg" style="width: 100px;height:100px;">
+	                </div>
+	                <div class="col-sm-9">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+	                  <small>Someone famous</small>
+	                </div>
+	              </div>
+	            </blockquote>
+	          </div>
+	          <!-- Quote 3 -->
+	          <div class="item">
+	            <blockquote>
+	              <div class="row">
+	                <div class="col-sm-3 text-center">
+	                  <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/keizgoesboom/128.jpg" style="width: 100px;height:100px;">
+	                </div>
+	                <div class="col-sm-9">
+	                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
+	                  <small>Someone famous</small>
+	                </div>
+	              </div>
+	            </blockquote>
+	          </div>
+	        </div>
+	        
+	        <!-- Carousel Buttons Next/Prev -->
+	        <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
+	        <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
+		</div>
+	</div>
